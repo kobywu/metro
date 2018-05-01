16c637f68e5ddcd3ed7543c44c62469ec4455d00
Koby <koby_wu@davincischools.org>
2018-05-01 14:00:31 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -31,5 +31,5 @@ var str = string.split("");
 var el = document.getElementById('str');
 (function animate() {
 str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
-var running = setTimeout(animate, 120);
+var running = setTimeout(animate, 200);
 })();
