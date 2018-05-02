d444c5ae201c205fecfa76130ef63b9b858d4c0b
Koby <koby_wu@davincischools.org>
2018-05-01 20:48:17 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -33,3 +33,67 @@ var el = document.getElementById('str');
 str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
 var running = setTimeout(animate, 200);
 })();
+
+// Get the modal
+var modal1 = document.getElementById('myModal1');
+// Get the button that opens the modal
+var btn1 = document.getElementById("button1");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal1.style.display = "none";
+}
+
+
+// Get the modal
+var modal2 = document.getElementById('myModal2');
+// Get the button that opens the modal
+var btn2 = document.getElementById("button2");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal2.style.display = "none";
+}
+
+
+
+
+// Get the modal
+var modal3 = document.getElementById('myModal3');
+// Get the button that opens the modal
+var btn3 = document.getElementById("button3");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close3")[0];
+// When the user clicks the button, open the modal 
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal3.style.display = "none";
+}
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+	 if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+	 if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
