1b50ef806a2ad9613e3deb7905c2c65d45385dc8
Koby <koby_wu@davincischools.org>
2018-05-29 14:12:33 -0700

    Add files via upload

---
+++ quiz-script.js
@@ -0,0 +1,144 @@
+//this will be on the button/options on the quiz. When the user hovers over these, this function will add this class to change the color
+function hover(element) {
+	 element.classList.add("hover");
+}
+//this will be on the button/options on the quiz. When the user doesnt hovers over these, this function will remove this class to change back the color
+function nohover(element){
+	 element.classList.remove("hover");
+}
+
+//These are the questions that are going to be asked
+var questions =[{
+	"question": "How much is a Student (K-8 and 9-12) 30-Day Pass?",
+	"option1":"$30",
+	"option2":"$24",
+	"option3":"$15",
+	"option4":"$36",
+	"answer":"2"
+},{
+	"question": "How much is a Student (K-8 and 9-12) Single One-Way Fare?",
+	"option1":"$1",
+	"option2":"$1.50",
+	"option3":"$0.75",
+	"option4":"2",
+	"answer":"1"
+},{
+	"question": "What are students required to have to purchase fare or ride with a Student TAP card?",
+	"option1":"valid photo ID",
+	"option2":"pepper spray",
+	"option3":"a homework assignment",
+	"option4":"a smile",
+	"answer":"1"
+},{
+	"question": "For additional information about reduced fare programs or questions about Student TAP cards contact...",
+	"option1":"Your teacher",
+	"option2":"Metro headquarters",
+	"option3":"Transit Information",
+	"option4":"metro Reduce Fare office",
+	"answer":"4"
+},{
+	"question": "What area do you HAVE to be in to get reduce fare at the LA Metro",
+	"option1":"school must be in the Los Angeles County",
+	"option2":"need to live in the Los Angeles County",
+	"option3":"You must go to school in Hawthorne",
+	"option4":"El Seugundo born and raised",
+	"answer":"1"
+},{
+	"question": "What other types of documentation can you use to get a TAP card?",
+	"option1":"your report card",
+	"option2":"car bills",
+	"option3":"a reciept at a store",
+	"option4":"your phone number",
+	"answer":"1"
+},{
+	"question": "Do you need to create a TAP account for each student?",
+	"option1":"Nope",
+	"option2":"Of course not",
+	"option3":"i am not sure",
+	"option4":"Yes",
+	"answer":"4"
+},{
+	"question": "What languages are not translated in the applications?",
+	"option1":"English",
+	"option2":"French",
+	"option3":"Hindi",
+	"option4":"Hmong",
+	"answer":"3"
+},{
+	"question": "How much cheaper is a Student K-12 TAP card than a regular one per ride?",
+	"option1":"$2",
+	"option2":"$1",
+	"option3":"$0.50",
+	"option4":"$0.75",
+	"answer":"4"
+},{
+	"question": "If you get a Student 30-Day Pass, how mnay rides do you get?",
+	"option1":"30",
+	"option2":"unlimited",
+	"option3":"70",
+	"option4":"60",
+	"answer":"2"
+},
+]
+//These variables will start at these values 
+var currentQuestion =0;
+var score = 0;
+var totQuestions = questions.length;
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButon = document.getElementById('nextbutton');
+var resultCont = document.getElementById('result');
+var redo = document.getElementById('redo');
+
+//this function will be shown when the page loads when it gets called at the way bottom
+function loadQuestion (questionIndex) {
+	var q = questions[questionIndex];
+	questionEl.textContent =(questionIndex + 1) + '.' + q.question; //This will start with the first question by the question index starting at 0. Then it will keep adding 1 for the next question
+	opt1.textContent = q.option1; //these options that are stated at the question variable will be displayed for the question that they go with
+	opt2.textContent = q.option2;
+	opt3.textContent = q.option3;
+	opt4.textContent = q.option4;
+};
+
+//This function will load the next question when you click on the next button
+function loadNext() {
+	var selectedOption = document.querySelector('input[type=radio]:checked'); //this will check if the user clicked on one of the options
+	if(!selectedOption){
+		alert('Please select your answer!'); //If the user did not click on one of the options, have a screen pop up and say something
+		return; //these words will be shown
+	}
+	var answer = selectedOption.value; 
+	if(questions[currentQuestion].answer == answer){ //if the selected option was actually the correct answer, then the score value will add 1
+		score += 1;
+	}
+	selectedOption.checked = false; 
+	currentQuestion++; //then the currentQuestion variable that is displayed will add one to the next
+	if(currentQuestion == totQuestions - 1){  //when the currect question number value is 1 less than the total number of questions then it will state "finished " on the next button istead. It is 1 less because the value the variable starts with is 0
+		nextButton.textContent ='Finish'; //The next button will no longer say "next question" by "finish"
+	}
+	if(currentQuestion == totQuestions){ //when the currectquestion equals the total amount of questions, which is when the user clicks on the finish button, this will happen
+		container.style.display = 'none'; //Thecontainer that holds the quiz question and options will no longer appear with the display value at none
+		resultCont.style.display =''; //then the end score container will change the display value from none to nothing which will show the container
+		resultCont.textContent = 'Your Score: ' + score; // the score will then be displayed with the words "your score"
+		return; //then those words will be shown
+	}
+	loadQuestion(currentQuestion); //this function will be called when this function is called by the user clicking on the HTML button that has the loadNext function on click
+}
+
+function refreshPage(){ //When the user clicks on the HTML refresh button, the website will refresh and then the user can restart the quiz
+    window.location.reload();
+} 
+//This is where the code is actually called to load and display the questions.
+loadQuestion(currentQuestion);
+
+function cbChange(obj) {
+    var cbs = document.getElementsByClassName("cb");
+    for (var i = 0; i < cbs.length; i++) {
+        cbs[i].checked = false;
+    }
+    obj.checked = true;
+}
